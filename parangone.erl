36094f414c6c703cbf25a7c987ea250bd1666439
Nicolas P. M. Legrand <nlegrand@ethelred.fr>
2010-11-08 23:47:22 +0100

    More abstraction

--- parangone.erl
+++ parangone.erl
@@ -1,34 +1,42 @@
 -module(parangone).
--export([http/1,stress/2,loop/0,start/0, rpc/2]).
+-export([http/1,start/0, rpc/2,timecount/1,stress/3,httpcount200/1]).
 
 start() ->
     register(parangon, spawn(fun() -> loop() end)).
 
-http(Url) ->
+timecount(Fun) ->
     statistics(wall_clock),
-    http:request("http://" ++ Url),
+    Result = Fun(),
     {_, Time} = statistics(wall_clock),
-    Time.
+    {Time, Result}.
 
-stress(Url, 1)    ->
-    http(Url);
-stress(Url, Time) ->
-    http(Url) + stress(Url, Time - 1).
+stress(_, 0, List)    ->
+    List;
+stress(Fun, Time, List) ->
+    stress(Fun, Time - 1, [timecount(Fun)|List]).
 
-rpc(Request, Time) ->
-    parangon ! {self(), Request, Time},
+rpc(Fun, Time) ->
+    parangon ! {self(), Fun, Time},
     receive
         {parangon, Response} ->
             Response
     end.
 
-
 loop() ->
     receive
-        {From, Url, Time} ->
-            From ! {parangon, stress(Url, Time) / Time},
+        {From, Fun, Time} ->
+            From ! {parangon, stress(Fun, Time, [])},
             loop()
 
     end.
 
-    
+http(Url) ->
+    fun() ->
+	    {_,{{_,Return,_},_,_}} = httpc:request(Url),
+	    Return
+    end.
+
+httpcount200([]) ->
+    0 ;
+httpcount200([{Time, 200}|T]) ->
+    Time + httpcount200(T).
