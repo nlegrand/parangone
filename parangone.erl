a17abcb60c2f6b2bfad1fe2321471373f1e51b9a
Nicolas P. M. Legrand <nlegrand@ethelred.fr>
2010-11-16 00:20:41 +0100

    move from self define server to gen_server
    rather ugly, unclean templating, internals should be rewritten,
    but works with gen_server now, so it's on his way to a cleaner
    interface

--- parangone.erl
+++ parangone.erl
@@ -1,8 +1,165 @@
+%%%-------------------------------------------------------------------
+%%% @author Nicolas P. M. Legrand <nlegrand@ethelred.fr>
+%%% @copyright (C) 2010, Nicolas P. M. Legrand
+%%%
+%%% Permission to use, copy, modify, and distribute this software for any
+%%% purpose with or without fee is hereby granted, provided that the above
+%%% copyright notice and this permission notice appear in all copies.
+%%%
+%%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+%%% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+%%% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.q
+%%%
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15 Nov 2010 by Nicolas P. M. Legrand <nlegrand@ethelred.fr>
+%%%-------------------------------------------------------------------
 -module(parangone).
--export([http/1,start/0, rpc/2,timecount/1,stress/2,sort_response/1]).
 
+-behaviour(gen_server).
+
+%% API
+-export([start/0,stop/0,stresssss/2]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3, http/1]).
+-compile(export_all).
+
+-define(SERVER, ?MODULE). 
+
+%-record(state, {}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
 start() ->
-    register(parangon, spawn(fun() -> loop() end)).
+    gen_server:start_link({local, ?MODULE}, ?MODULE, [], []).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Stops the server
+%%
+%% @spec stop() -> {ok, Dict}
+%% @end
+%%--------------------------------------------------------------------
+
+stop() -> gen_server:call(?MODULE, stop).
+
+stresssss(Fun, Time) -> gen_server:call(?MODULE, {Fun, Time}).
+
+state() ->
+    gen_server:call(?MODULE, state).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    {ok, dict:new()}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call({Fun, Time}, _From, State) ->
+    ResList = stress(Fun, Time),
+    NewState = sort_response(ResList, State),
+    {reply, ok, NewState} ;
+handle_call(state, _From, State) ->
+    {reply, State, State} ;
+handle_call(stop, _From, State) ->
+    {stop, normal, stopped, State}.
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
 
 timecount(Fun) ->
     statistics(wall_clock),
@@ -18,28 +175,14 @@ stress(_, 0, List) ->
 stress(Fun, Time, List) ->
     stress(Fun, Time - 1, [timecount(Fun)|List]).
 
-rpc(Fun, Time) ->
-    parangon ! {self(), Fun, Time},
-    receive
-        {parangon, Node, Response} ->
-            {Node, Response}
-    end.
-
-loop() ->
-    receive
-        {From, Fun, Time} ->
-            From ! {parangon, node(), stress(Fun, Time, [])},
-            loop()
-    end.
-
 http(Url) ->
     fun() ->
 	    {_,{{_,Return,_},_,_}} = httpc:request(Url),
 	    Return
     end.
 
-sort_response(Response_List) ->
-	       sort_response(Response_List, dict:new()).
+%sort_response(Response_List) ->
+%	       sort_response(Response_List, dict:new()).
 
 sort_response([], Dict) ->
     dict:map(fun(_, List)
